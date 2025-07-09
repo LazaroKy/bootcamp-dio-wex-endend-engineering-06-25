1fd90ecc185d9872d0db0a3c69340abda6be05f1
LázaroK <lazarolk23@hotmail.com>
2025-07-09 14:49:40 -0300

    feat: declare variables and assign initial values

--- TipoDeDados/Program.cs
+++ TipoDeDados/Program.cs
@@ -1,2 +1,14 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿//tipo nome = valor;
+string apresentacao = "Olá, seja bem-vindo";
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.80M;
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00")); 
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
