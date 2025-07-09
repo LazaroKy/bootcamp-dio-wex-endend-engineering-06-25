6e97f0da834a1a90cd01854ad4e4be92506366b6
LázaroK <lazarolk23@hotmail.com>
2025-07-09 17:37:57 -0300

    feat: use arithmetic operators

---
+++ Operadores/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int a = 10;
+int b = 20;
+int c = a + b;
+
+Console.WriteLine(c);
+
+c += 5; // c = c + 5;
+Console.WriteLine(c);
+c -= 10; //c = c -10;
+Console.WriteLine(c);
+c /= 5;//c = c /5;
+Console.WriteLine(c);
+c *= 4; //c = c *4;
+Console.WriteLine(c);
