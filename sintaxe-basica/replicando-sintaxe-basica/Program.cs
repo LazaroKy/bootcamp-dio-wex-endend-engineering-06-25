f25c08bcd8985a3435663757f493cbeeff91756f
LázaroK <lazarolk23@hotmail.com>
2025-07-09 14:04:52 -0300

    feat: add user input with ReadLine

--- sintaxe-basica/replicando-sintaxe-basica/Program.cs
+++ sintaxe-basica/replicando-sintaxe-basica/Program.cs
@@ -2,6 +2,8 @@
 using replicando_sintaxe_basica.Models;
 
 Person person = new Person();
-person.Name ="Alan";
-person.Age = 10;
+Console.WriteLine("Digite seu nome: ");
+person.Name = Console.ReadLine();
+Console.WriteLine("Digite sua idade: ");
+person.Age = Convert.ToInt32(Console.ReadLine());
 person.Apresentar();
