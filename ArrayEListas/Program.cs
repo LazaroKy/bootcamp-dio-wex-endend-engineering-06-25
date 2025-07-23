feff509349647e617f612118a63ce9481bab860a
LázaroK <lazarolk23@hotmail.com>
2025-07-23 02:12:54 -0300

    feat: work with int array using for loop

---
+++ ArrayEListas/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Arrays and Lists");
+
+int[] arrayDeInteiros = new int[3];
+//Inicializar usando já os valores {1,2,3,4,5,..}
+
+arrayDeInteiros[0] = 72;
+arrayDeInteiros[1] = 32;
+arrayDeInteiros[2] = 30;
+
+//arrayDeInteiros[3] = 1;  Erro IndexOutOfRangeException
+
+for (int contador = 0; contador < arrayDeInteiros.Length; contador++)
+{
+    Console.WriteLine($"Posição nº {contador} - {arrayDeInteiros[contador]}");
+}
