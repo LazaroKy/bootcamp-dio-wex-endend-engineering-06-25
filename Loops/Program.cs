2d8647b52cccca71dc2ca7100117d9631d4887c4
LázaroK <lazarolk23@hotmail.com>
2025-07-23 00:53:02 -0300

    feat: practice for loop

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.WriteLine("Estrutura de Repetição");
+
+//Loop For
+int numero = 5;
+
+for (int count = 0; count <= 10; count++)
+{
+    Console.WriteLine($"{count} x {numero} = {count * numero}");
+}
