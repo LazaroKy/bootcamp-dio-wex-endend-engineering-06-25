d126d5b3b18ba127beb9c8d9875e8a1e3e62d802
LázaroK <lazarolk23@hotmail.com>
2025-07-23 00:53:36 -0300

    feat: practice while loop

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -2,8 +2,15 @@
 
 //Loop For
 int numero = 5;
+// for (int count = 0; count <= 10; count++)
+// {
+//     Console.WriteLine($"{count} x {numero} = {count * numero}");
+// }
 
-for (int count = 0; count <= 10; count++)
+//While loop
+while (numero != 0)
 {
-    Console.WriteLine($"{count} x {numero} = {count * numero}");
+    Console.WriteLine(numero--);
+    Console.WriteLine("Break");
+    break;
 }
