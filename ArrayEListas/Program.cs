06015721cd2f94499eb68fdd64345611d3a2141e
LázaroK <lazarolk23@hotmail.com>
2025-07-23 02:17:50 -0300

    feat: work with int array using foreach loop

--- ArrayEListas/Program.cs
+++ ArrayEListas/Program.cs
@@ -8,8 +8,14 @@ arrayDeInteiros[1] = 32;
 arrayDeInteiros[2] = 30;
 
 //arrayDeInteiros[3] = 1;  Erro IndexOutOfRangeException
+// Console.WriteLine("Using for loop");
+// for (int contador = 0; contador < arrayDeInteiros.Length; contador++)
+// {
+//     Console.WriteLine($"Posição nº {contador} - {arrayDeInteiros[contador]}");
+// }
 
-for (int contador = 0; contador < arrayDeInteiros.Length; contador++)
+Console.WriteLine("Using foreach ");
+foreach (int valor in arrayDeInteiros)
 {
-    Console.WriteLine($"Posição nº {contador} - {arrayDeInteiros[contador]}");
+    Console.WriteLine(valor);
 }
