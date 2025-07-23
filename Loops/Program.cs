22a6f584a471f02cb2550254d3d8a0dec882dc68
LázaroK <lazarolk23@hotmail.com>
2025-07-23 01:11:26 -0300

    feat: add simple app practicing while loop and switch-case

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Estrutura de Repetição");
 
 //Loop For
-int numero = 5;
+//int numero = 5;
 // for (int count = 0; count <= 10; count++)
 // {
 //     Console.WriteLine($"{count} x {numero} = {count * numero}");
@@ -16,8 +16,46 @@ int numero = 5;
 // }
 
 //Do while
-do
+// do
+// {
+//     numero--;
+//     Console.WriteLine($"Loop acaba no 0, agora o valor é {numero}");
+// } while (numero != 0);
+
+bool exibirMenu = true;
+while (exibirMenu)
 {
-    numero--;
-    Console.WriteLine($"Loop acaba no 0, agora o valor é {numero}");
-} while (numero != 0);
+    Console.Clear();
+    Console.WriteLine("Digite o número de acordo com a opção que deseja executar:");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    string opcao = Console.ReadLine();
+    switch (opcao)
+    {
+        case "1":
+            Console.WriteLine("Cadastrar cliente");
+            break;
+
+        case "2":
+            Console.WriteLine("Buscar cliente");
+            break;
+
+        case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+
+        case "4":
+            Console.WriteLine("Encerrar");
+            exibirMenu = false;
+            //Environment.Exit(0); Sai totalmente do programa
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+Console.WriteLine("Programa encerrado");
