64ed1021b4101cabf9126d87c2eca075e60e6254
LázaroK <lazarolk23@hotmail.com>
2025-07-27 02:32:11 -0300

    feat: collections - stack structure

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -1,24 +1,45 @@
 ﻿Console.WriteLine("Collections!");
 
 //Queue - FIFO
-Queue<int> queue = new Queue<int>();
+// Queue<int> queue = new Queue<int>();
 
-queue.Enqueue(2);
-queue.Enqueue(5);
-queue.Enqueue(7);
-queue.Enqueue(4);
+// queue.Enqueue(2);
+// queue.Enqueue(5);
+// queue.Enqueue(7);
+// queue.Enqueue(4);
 
-foreach (int item in queue)
-{
-    Console.WriteLine(item);
-}
+// foreach (int item in queue)
+// {
+//     Console.WriteLine(item);
+// }
 
-queue.Dequeue(); //remove first element
-queue.Enqueue(21);
+// queue.Dequeue(); //remove first element
+// queue.Enqueue(21);
 
-    Console.WriteLine("--------------");
+//     Console.WriteLine("--------------");
 
-foreach (int item in queue)
-{
-    Console.WriteLine(item);
-}
+// foreach (int item in queue)
+// {
+//     Console.WriteLine(item);
+// }
+
+//Stack - LIFO
+
+Stack<int> stack = new Stack<int>();
+stack.Push(1);
+stack.Push(2);
+stack.Push(3);
+stack.Push(4);
+ foreach (int item in stack)
+ {
+     Console.WriteLine(item);
+ }
+
+Console.WriteLine("-------");
+stack.Pop();
+stack.Push(5);
+Console.WriteLine("-------");
+ foreach (int item in stack)
+ {
+     Console.WriteLine(item);
+ }
