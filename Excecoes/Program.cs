369a4cf95c790fcc46343313950349e69df62e32
LázaroK <lazarolk23@hotmail.com>
2025-07-27 02:06:12 -0300

    feat: handle runtime error using try-catch blocks

---
+++ Excecoes/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Execeções");
+
+
+try
+{
+    //Com um path errado, uma exceção é disparada em runtime
+    string[] linhas = File.ReadAllLines("arquivaoLeitura.txt");
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (FileNotFoundExeception ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo, erro:  {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica, erro: {ex.Message}");
+}
