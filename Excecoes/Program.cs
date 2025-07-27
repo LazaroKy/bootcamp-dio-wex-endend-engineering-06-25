aa68395811ed23c4fc5e6add96b39ae1c96f04b7
LázaroK <lazarolk23@hotmail.com>
2025-07-27 02:15:38 -0300

    feat: use throw statement

--- Excecoes/Program.cs
+++ Excecoes/Program.cs
@@ -1,21 +1,23 @@
-﻿
+﻿using Excecoes.Models;
 Console.WriteLine("Execeções");
 
 
-try
-{
-    //Com um path errado, uma exceção é disparada em runtime
-    string[] linhas = File.ReadAllLines("arquivaoLeitura.txt");
-    foreach (string linha in linhas)
-    {
-        Console.WriteLine(linha);
-    }
-}
-catch (FileNotFoundExeception ex)
-{
-    Console.WriteLine($"Ocorreu um erro na leitura do arquivo, erro:  {ex.Message}");
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Ocorreu uma exceção genérica, erro: {ex.Message}");
-}
+// try
+// {
+//     //Com um path errado, uma exceção é disparada em runtime
+//     string[] linhas = File.ReadAllLines("arquivaoLeitura.txt");
+//     foreach (string linha in linhas)
+//     {
+//         Console.WriteLine(linha);
+//     }
+// }
+// catch (FileNotFoundExeception ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo, erro:  {ex.Message}");
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu uma exceção genérica, erro: {ex.Message}");
+// }
+
+new ExemploException().Metodo1();
