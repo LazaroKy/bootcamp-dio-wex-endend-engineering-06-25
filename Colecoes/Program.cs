6ff564d176c538eb93f1c8b811048bb623a866a7
LázaroK <lazarolk23@hotmail.com>
2025-07-27 02:41:08 -0300

    feat: collections - dictionary structure

--- Colecoes/Program.cs
+++ Colecoes/Program.cs
@@ -24,22 +24,41 @@
 // }
 
 //Stack - LIFO
+// Stack<int> stack = new Stack<int>();
+// stack.Push(1);
+// stack.Push(2);
+// stack.Push(3);
+// stack.Push(4);
+//  foreach (int item in stack)
+//  {
+//      Console.WriteLine(item);
+//  }
 
-Stack<int> stack = new Stack<int>();
-stack.Push(1);
-stack.Push(2);
-stack.Push(3);
-stack.Push(4);
- foreach (int item in stack)
- {
-     Console.WriteLine(item);
- }
+// Console.WriteLine("-------");
+// stack.Pop();
+// stack.Push(5);
+// Console.WriteLine("-------");
+//  foreach (int item in stack)
+//  {
+//      Console.WriteLine(item);
+//  }
 
-Console.WriteLine("-------");
-stack.Pop();
-stack.Push(5);
-Console.WriteLine("-------");
- foreach (int item in stack)
- {
-     Console.WriteLine(item);
- }
+Dictionary<string,int> dictionary = new Dictionary<string,int>();
+
+dictionary.Add("Cimento", 12);
+dictionary.Add("Brita", 14);
+dictionary.Add("Ferro", 9);
+
+foreach (var item in dictionary)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine("--------");
+
+dictionary["Brita"] = 20;
+foreach (var item in dictionary)
+{
+    Console.WriteLine(item);
+}
+
+bool temBrita =dictionary.ContainsKey("Brita");
