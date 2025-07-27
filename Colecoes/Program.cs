eb69e3286f56094fe5c9b04240e3723f19f70117
LázaroK <lazarolk23@hotmail.com>
2025-07-27 02:26:14 -0300

    feat: collections - queue structure

---
+++ Colecoes/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Collections!");
+
+//Queue - FIFO
+Queue<int> queue = new Queue<int>();
+
+queue.Enqueue(2);
+queue.Enqueue(5);
+queue.Enqueue(7);
+queue.Enqueue(4);
+
+foreach (int item in queue)
+{
+    Console.WriteLine(item);
+}
+
+queue.Dequeue(); //remove first element
+queue.Enqueue(21);
+
+    Console.WriteLine("--------------");
+
+foreach (int item in queue)
+{
+    Console.WriteLine(item);
+}
