e6e6415aee2d6056c970caa42544e312a05a745e
LázaroK <lazarolk23@hotmail.com>
2025-07-29 23:16:20 -0300

    feat: mark Aluno class as sealed and seal Person.Apresentar method

--- POO/Models/Aluno.cs
+++ POO/Models/Aluno.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace POO.Models
 {
-    public class Aluno : Pessoa
+    public sealed class Aluno : Pessoa
     {
         public Aluno(string nome) :base(nome)
         {
@@ -13,9 +13,9 @@ namespace POO.Models
         public double Nota { get; set; }
 
         
-        public override void Apresentar()
-        {
-            Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos e sou um aluno nota {Nota}");
-        }
+        // public override void Apresentar()
+        // {
+        //     Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos e sou um aluno nota {Nota}");
+        // }
     }
 }
