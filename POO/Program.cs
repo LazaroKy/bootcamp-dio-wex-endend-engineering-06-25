f57225db72b9747839a3451ddda657a4eb3cac06
LázaroK <lazarolk23@hotmail.com>
2025-07-29 22:57:50 -0300

    feat: implement class and method abstract

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,14 +1,18 @@
 ﻿using POO.Models;
 //Objeto
-Pessoa pessoa = new Pessoa();
+// Pessoa pessoa = new Pessoa();
 
-pessoa.Nome = "Alan";
-pessoa.Idade = 10;
-pessoa.Apresentar();
+// pessoa.Nome = "Alan";
+// pessoa.Idade = 10;
+// pessoa.Apresentar();
 
-Aluno aluno = new Aluno();
-aluno.Nome = "Pedro";
-aluno.Idade = 8;
-aluno.Nota = 10;
+// Aluno aluno = new Aluno();
+// aluno.Nome = "Pedro";
+// aluno.Idade = 8;
+// aluno.Nota = 10;
 
-aluno.Apresentar();
+// aluno.Apresentar();
+
+ContaCorrente c = new ContaCorrente();
+c.Creditar(200);
+c.ExibirSaldo();
