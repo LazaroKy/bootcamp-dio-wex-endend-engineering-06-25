e4267fce52dd5707e9f0272c7e303fada46142b1
LázaroK <lazarolk23@hotmail.com>
2025-07-31 00:23:19 -0300

    feat: create User controller with ObterDataHoraAtual endpoint

---
+++ PrimeiraAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc; // Importa classes necessárias p Controller
+
+namespace PrimeiraAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+
+        [HttpGet("ObterDataHoraAtual")] //Não precisa ser o mesmo nome do método, isso seria o path
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortDateString()
+            };
+
+            return Ok(obj);
+        }
+    }
+}
