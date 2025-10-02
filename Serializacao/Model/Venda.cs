c6b0a5b540ce58808f73597d429c991d50767b52
LázaroK <lazarolk23@hotmail.com>
2025-10-02 02:34:38 -0300

    feat: implement JSON deserialization and observe ISO 8601 date

--- Serializacao/Model/Venda.cs
+++ Serializacao/Model/Venda.cs
@@ -8,14 +8,21 @@ namespace Serializacao.Model
     public class Venda
     {
 
-        public Venda(int id, string produto, decimal preco)
+        public Venda(int id, string produto, decimal preco, DateTime data_venda)
         {
             Id = id;
             Produto = produto;
             Preco = preco;
+            DataVenda = data_venda;
         }
         public int Id { get; set; }
         public string Produto { get; set; }
         public decimal Preco { get; set; }
+        public DateTime DataVenda { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Produto: {Produto}, Preço: {Preco}, DataVenda: {DataVenda}";
+        }
     }
 }
--- Serializacao/Program.cs
+++ Serializacao/Program.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 
 Console.WriteLine("Serializandoo!");
 
-Venda v1 = new Venda(1, "Pé de cabra", 97M);
+Venda v1 = new Venda(1, "Pé de cabra", 97M, DateTime.Now);
 //string slzed = JsonConvert.SerializeObject(v1);
 //Para melhor visualização você pode usar: 
 string slzed = JsonConvert.SerializeObject(v1, Formatting.Indented);
@@ -14,7 +14,7 @@ Console.WriteLine(slzed);
 File.WriteAllText("Files/venda1.json", slzed);
 
 //Serializando coleções 
-Venda v2 = new Venda(2, "Tijolo", 47M);
+Venda v2 = new Venda(2, "Tijolo", 47M, DateTime.Now);
 
 List<Venda> vendas = new List<Venda>();
 vendas.Add(v1);
@@ -24,4 +24,15 @@ string serializa = JsonConvert.SerializeObject(vendas, Formatting.Indented);
 
 File.WriteAllText("Files/vendas.json", serializa);
 
+//As datas são salvas de acordo com a ISO 8601 
+//JSON não segue um formato de data integrado, usam o formato de string de acordo com a ISO
+//onde combina data e hora em um formato padrão, como AAAA-MM-DDTHH:MM:SSZ
 
+//Deserializando objetos - vc tem o arquivo e quer criar o objeto
+
+string objeto_deserializado = File.ReadAllText("Files/venda3.json");
+//Você deve observar a estrutura do seu json e abstrair no seu objeto
+Venda v3 = JsonConvert.DeserializeObject<Venda>(objeto_deserializado);
+//É preciso adiconar (sobreescrever na vdd) o ToString da sua classe para que ele imprima de fato o objeto deserializado, e não o namespace e a classe do tipo
+
+Console.WriteLine(v3);
